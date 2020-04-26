38190d2989161c992432c21eec3e290b42394fe1
Darren Cheng <darren@thanx.com>
2020-04-25 19:26:41 -0700

    Iterate through BeastSaber bookmarks.

--- lib/beast_saber_sync.rb
+++ lib/beast_saber_sync.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'interactor'
+require 'faraday'
+require 'faraday_middleware'
+
 # Primary entry-point into syncing logic.
 # * fetches songs from specified BeastSaber user's bookmarks
 # * fetches songs already installed
@@ -8,7 +12,33 @@
 # * for songs not yet installed - install
 # * for songs no longer bookmarked - remove
 class BeastSaberSync
-  def self.run
+  include Interactor
+
+  before do
+    context.username ||= 'sanguinerane'
+  end
+
+  def call
     puts 'Syncing...'
+    songs
+  end
+
+private
+
+  def songs
+    response = client.get('songs', bookmarked_by: context.username)
+    response.body['songs'].each do |song|
+      puts "#{song['song_key']} - #{song['hash']} - #{song['title']}"
+    end
+  end
+
+  def client
+    @client ||= Faraday.new(
+      url: 'https://bsaber.com/wp-json/bsaber-api/'
+    ) do |faraday|
+      faraday.request  :json
+      faraday.response :json, content_type: /\bjson$/
+      faraday.adapter  :net_http
+    end
   end
 end
