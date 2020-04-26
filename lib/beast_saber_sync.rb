40bc7a99382850823bab9dc3bb9d5c8f4da1a2d8
Darren Cheng <darren@thanx.com>
2020-04-26 10:46:29 -0700

    Use hash instead of key as lookup.

--- lib/beast_saber_sync.rb
+++ lib/beast_saber_sync.rb
@@ -41,13 +41,12 @@ private
 
   def load_bookmarks!
     beast_saber.each_song do |song|
-      key = song['song_key']
-
-      context.data[key] ||= {}
-      context.data[key][:bookmarked] = true
-      context.data[key][:title] = song['title']
-      context.data[key][:hash] = song['hash']
-      context.data[key][:author] = song['level_author_name']
+      hash = song['hash']
+      context.data[hash] ||= {}
+      context.data[hash][:bookmarked] = true
+      context.data[hash][:key] = song['song_key']
+      context.data[hash][:title] = song['title']
+      context.data[hash][:author] = song['level_author_name']
     end
   end
 
